366fde92c690de25bf17c34c18a98fa4ada9df11
Aditya Mishra <mishraaditya3456@gmail.com>
2025-05-23 03:55:24 +0530

    deployment testing

---
+++ js/siteData.js
@@ -0,0 +1,274 @@
+const siteData = {
+
+  culthist: [
+    {
+      id: "tirunelli",
+      name: "Tirunelli Temple",
+      description: "Ancient temple dedicated to Lord Vishnu, nestled in the hills of Wayanad.",
+      image: "assets/img/tirunelli.png",
+      holidays: "Sun",
+      timing: "10:00-18:00",
+      Contact: "+91-000 000 0000",
+      locationid: "1",
+      knowmore: "./knowmore.html"
+    },
+    {
+      id: "mosque1",
+      name: "Varampatta mosque",
+      description: "Historic Varampatta mosque with beautiful architecture and cultural significance.",
+      image: "assets/img/ghats_wayanad.jpg",
+      holidays: "Sun",
+      timing: "10:00-18:00",
+      Contact: "+91-000 000 0000",
+      locationid: "1",
+      knowmore: "./knowmore.html"
+    },
+    {
+      id: "temple2",
+      name: "Jain Temple",
+      description: "Historic Jain temple with beautiful architecture and cultural significance.",
+      image: "assets/img/ghats_wayanad.jpg",
+      holidays: "Sun",
+      timing: "10:00-18:00",
+      Contact: "+91-000 000 0000",
+      locationid: "1",
+      knowmore: "./knowmore.html"
+    },
+    {
+      id: "temple3",
+      name: "Seetha Lava Kusha Temple",
+      description: "Temple associated with the epic Ramayana, located in a serene setting.",
+      image: "assets/img/wayanad_mist.jpg",
+      holidays: "Sun",
+      timing: "10:00-18:00",
+      Contact: "+91-000 000 0000",
+      locationid: "1",
+      knowmore: "./knowmore.html"
+    }
+  ],
+  waterfalls: [
+    {
+      id: "meenmutty",
+      name: "Meenmutty Falls",
+      description: "One of the largest and most spectacular waterfalls in Wayanad.",
+      image: "assets/img/falls/meenmufalls.png",
+      holidays: "Sun",
+      timing: "10:00-18:00",
+      Contact: "+91-000 000 0000",
+      locationid: "1",
+      knowmore: "./knowmore.html"
+    },
+    {
+      id: "soochipara",
+      name: "Soochipara Falls",
+      description: "Also known as Sentinel Rock Waterfalls, perfect for trekking and rock climbing.",
+      image: "assets/img/falls/soojipara_falls_b_w_wayanad.jpg",
+      holidays: "Sun",
+      timing: "10:00-18:00",
+      Contact: "+91-000 000 0000",
+      locationid: "1",
+      knowmore: "./knowmore.html"
+    },
+    {
+      id: "kanthanpara",
+      name: "Kanthanpara Falls",
+      description: "A picturesque waterfall surrounded by lush greenery and coffee plantations.",
+      image: "assets/img/falls/kanta_falls.png",
+      holidays: "Sun",
+      timing: "10:00-18:00",
+      Contact: "+91-000 000 0000",
+      locationid: "1",
+      knowmore: "./knowmore.html"
+    }
+  ],
+  museums: [
+    {
+      id: "folklore",
+      name: "Folklore Museum",
+      description: "Showcases the rich cultural heritage and tribal artifacts of Wayanad.",
+      image: "assets/img/tirunelli.png",
+      holidays: "Sun",
+      timing: "10:00-18:00",
+      Contact: "+91-000 000 0000",
+      locationid: "1",
+      knowmore: "./knowmore.html"
+    },
+    {
+      id: "edakkal",
+      name: "Edakkal Caves",
+      description: "Ancient rock shelter with prehistoric carvings and inscriptions.",
+      image: "assets/img/edakkal_caves.webp",
+      holidays: "Sun",
+      timing: "10:00-18:00",
+      Contact: "+91-000 000 0000",
+      locationid: "1",
+      knowmore: "./knowmore.html"
+    },
+    {
+      id: "ambalavayal",
+      name: "Ambalavayal Heritage Museum",
+      description: "Displays tribal heritage and ancient artifacts from the region.",
+      image: "assets/img/tea_wayanad.webp",
+      holidays: "Sun",
+      timing: "10:00-18:00",
+      Contact: "+91-000 000 0000",
+      locationid: "1",
+      knowmore: "./knowmore.html"
+    }
+  ],
+  romantic: [
+    {
+      id: "karapuzhadam",
+      name: "Karapuzhs Dam",
+      description: "Karapuzhs Dam.",
+      image: "assets/img/falls/meenmufalls.png",
+      holidays: "Sun",
+      timing: "10:00-18:00",
+      Contact: "+91-000 000 0000",
+      locationid: "1",
+      knowmore: "./knowmore.html"
+    },
+    {
+      id: "PookodeLake",
+      name: "Pookode Lake",
+      description: "Also known as Sentinel Rock Waterfalls, perfect for trekking and rock climbing.",
+      image: "assets/img/falls/soojipara_falls_b_w_wayanad.jpg",
+      holidays: "Sun",
+      timing: "10:00-18:00",
+      Contact: "+91-000 000 0000",
+      locationid: "1",
+      knowmore: "./knowmore.html"
+    },
+    {
+      id: "banasurasagardam",
+      name: "Banasurasagar Dam",
+      description: "A picturesque waterfall surrounded by lush greenery and coffee plantations.",
+      image: "assets/img/falls/kanta_falls.png",
+      holidays: "Sun",
+      timing: "10:00-18:00",
+      Contact: "+91-000 000 0000",
+      locationid: "1",
+      knowmore: "./knowmore.html"
+    }
+  ],
+  plantation: [
+    {
+      id: "plantation",
+      name: "Harissons",
+      description: "Showcases the rich cultural heritage and tribal artifacts of Wayanad.",
+      image: "assets/img/plantation/tea_plantaion.jpg",
+      holidays: "Sun",
+      timing: "10:00-18:00",
+      Contact: "+91-000 000 0000",
+      locationid: "1",
+      knowmore: "./knowmore.html"
+    },
+    {
+      id: "teaestate",
+      name: "Wayanad Tea Estate",
+      description: "Expansive tea plantations with guided tours and tasting sessions.",
+      image: "assets/img/plantation/tea_estate.webp",
+      holidays: "Mon",
+      timing: "09:00-17:00",
+      Contact: "+91-111 111 1111",
+      locationid: "2",
+      knowmore: "./knowmore.html"
+    }
+  ],
+  trucking: [
+    {
+      id: "Chundale",
+      name: "chundale",
+      description: "Showcases the rich cultural heritage and tribal artifacts of Wayanad.",
+      image: "assets/img/trucking/edakkal_caves.webp",
+      holidays: "Sun",
+      timing: "10:00-18:00",
+      Contact: "+91-000 000 0000",
+      locationid: "1",
+      knowmore: "./knowmore.html"
+    },
+    {
+      id: "chembra",
+      name: "Chembra Peak Trek",
+      description: "Popular trekking destination with breathtaking views and a heart-shaped lake.",
+      image: "assets/img/trucking/trucking.jpg",
+      holidays: "None",
+      timing: "06:00-17:00",
+      Contact: "+91-222 222 2222",
+      locationid: "3",
+      knowmore: "./knowmore.html"
+    }
+  ],
+  wildlife: [
+    {
+      id: "Bandipur",
+      name: "Bandipur Museum",
+      description: "Showcases the rich cultural heritage and tribal artifacts of Wayanad.",
+      image: "assets/img/wildlife/Wildlife_safari_at_tholpetty_wildlife_sanctuaryjpg.avif",
+      holidays: "Sun",
+      timing: "10:00-18:00",
+      Contact: "+91-000 000 0000",
+      locationid: "1",
+      knowmore: "./knowmore.html"
+    },
+    {
+      id: "muthanga",
+      name: "Muthanga Wildlife Sanctuary",
+      description: "Sanctuary with elephants, deer, and a variety of birds in their natural habitat.",
+      image: "assets/img/wildlife/pakshipathalam-bird-sanctuary-wayanad.jpg",
+      holidays: "None",
+      timing: "07:00-17:00",
+      Contact: "+91-333 333 3333",
+      locationid: "4",
+      knowmore: "./knowmore.html"
+    }
+  ],
+  farms: [
+    {
+      id: "ambalavayal",
+      name: "Ambalavayal",
+      description: "Showcases the rich cultural heritage and tribal artifacts of Wayanad.",
+      image: "assets/img/tirunelli.png",
+      holidays: "Sun",
+      timing: "10:00-18:00",
+      Contact: "+91-000 000 0000",
+      locationid: "1",
+      knowmore: "./knowmore.html"
+    },
+    {
+      id: "pepperfarm",
+      name: "Pepper Farm",
+      description: "Experience spice cultivation and farm life in Wayanad.",
+      image: "assets/img/falls/chitafalls.png",
+      holidays: "None",
+      timing: "08:00-18:00",
+      Contact: "+91-444 444 4444",
+      locationid: "5",
+      knowmore: "./knowmore.html"
+    }
+  ],
+  food: [
+    {
+      id: "resturants",
+      name: "Resturants",
+      description: "Showcases the rich cultural heritage and tribal artifacts of Wayanad.",
+      image: "assets/img/tirunelli.png",
+      holidays: "Sun",
+      timing: "10:00-18:00",
+      Contact: "+91-000 000 0000",
+      locationid: "1",
+      knowmore: "./knowmore.html"
+    },
+    {
+      id: "localcuisine",
+      name: "Local Cuisine",
+      description: "Try authentic Kerala dishes at local eateries and food stalls.",
+      image: "assets/img/heritage/chain_tree.avif",
+      holidays: "None",
+      timing: "11:00-23:00",
+      Contact: "+91-555 555 5555",
+      locationid: "6",
+      knowmore: "./knowmore.html"
+    }
+  ]
+}
