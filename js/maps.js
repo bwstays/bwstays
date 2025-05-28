bb857e21d778cee318fecdee81857ea233827bae
bwstays <bwstays@gmail.com>
2025-05-28 11:50:28 +0530

    updated

--- js/maps.js
+++ js/maps.js
@@ -1,6 +1,6 @@
 
 
-var locations = [
+var bwlocations = [
 
     ['<h6><a target="_blank" href="https://www.dtpcwayanad.com/destination/pookode-lake-wayanad" title="Pookode Lake">Pookode Lake</a></h6><a target="_blank" href="https://www.dtpcwayanad.com/destination/pookode-lake-wayanad" title="Pookode Lake"><img alt=""  src="https://www.dtpcwayanad.com/uploads/picture_gallery/gallery_images/pookode-lake-wayanad-3-20230429174142824511.webp" width="300" ></a>', 11.542470, 76.027224, 2, "https://www.dtpcwayanad.com/images/pin-drop.png"],
 
@@ -22,15 +22,15 @@ var map = new google.maps.Map(document.getElementById('map'), {
 });
 var infowindow = new google.maps.InfoWindow();
 var marker, i;
-for (i = 0; i < locations.length; i++) {
+for (i = 0; i < bwlocations.length; i++) {
   marker = new google.maps.Marker({
-    position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-    icon: locations[i][4],
+    position: new google.maps.LatLng(bwlocations[i][1], bwlocations[i][2]),
+    icon: bwlocations[i][4],
     map: map
   });
   google.maps.event.addListener(marker, 'click', (function (marker, i) {
     return function () {
-      infowindow.setContent(locations[i][0]);
+      infowindow.setContent(bwlocations[i][0]);
       infowindow.open(map, marker);
     }
   })(marker, i));
