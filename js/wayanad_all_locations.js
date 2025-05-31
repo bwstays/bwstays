5d1352b442df4ab11d1c30aeab2067f45a48e305
Aditya Mishra <mishraaditya3456@gmail.com>
2025-05-31 16:50:30 +0530

    updated booking.html design

--- js/wayanad_all_locations.js
+++ js/wayanad_all_locations.js
@@ -1,9 +1,5 @@
-
-
 var locations = [
   ['<h6><a target="_blank" href="https://www.dtpcwayanad.com/destination/banasura-hills-wayanad" title="Banasura Hills">Banasura Hills</a></h6><a target="_blank" href="https://www.dtpcwayanad.com/destination/banasura-hills-wayanad" title="Banasura Hills"><img alt=""  src="https://www.dtpcwayanad.com/uploads/picture_gallery/gallery_images/banasura-hills-1920x1080-20230429161726128402.webp" width="300" ></a>', 11.695175204072623, 75.90917173969375, 2, "https://www.dtpcwayanad.com/images/pin-drop.png"],
-
-
 
   ['<h6><a target="_blank" href="https://www.dtpcwayanad.com/destination/brahmagiri-hills-wayanad" title="Brahmagiri Hills">Brahmagiri Hills</a></h6><a target="_blank" href="https://www.dtpcwayanad.com/destination/brahmagiri-hills-wayanad" title="Brahmagiri Hills"><img alt=""  src="https://www.dtpcwayanad.com/uploads/picture_gallery/gallery_images/brahmagiri-hills-wayanad-34-20230428184555850297.webp" width="300" ></a>', 11.932026, 75.994078, 2, "https://www.dtpcwayanad.com/images/pin-drop.png"],
 
@@ -15,111 +11,214 @@ var locations = [
 
   ['<h6><a target="_blank" href="https://www.dtpcwayanad.com/destination/korome-mosque-wayanad" title="Korome Mosque">Korome Mosque</a></h6><a target="_blank" href="https://www.dtpcwayanad.com/destination/korome-mosque-wayanad" title="Korome Mosque"><img alt=""  src="https://www.dtpcwayanad.com/uploads/picture_gallery/gallery_images/korome-mosque-1920x1080-20230429161843647002.webp" width="300" ></a>', 11.744250584874738, 75.88027044523321, 2, "https://www.dtpcwayanad.com/images/pin-drop.png"],
 
-
-
   ['<h6><a target="_blank" href="https://www.dtpcwayanad.com/destination/pallikunnu-church-wayanad" title="Pallikunnu Church">Pallikunnu Church</a></h6><a target="_blank" href="https://www.dtpcwayanad.com/destination/pallikunnu-church-wayanad" title="Pallikunnu Church"><img alt=""  src="https://www.dtpcwayanad.com/uploads/picture_gallery/gallery_images/pallikunnu-church-wayanad-15-20230429102451469670.webp" width="300" ></a>', 11.638232, 76.010695, 2, "https://www.dtpcwayanad.com/images/pin-drop.png"],
-
-
 
   ['<h6><a target="_blank" href="https://www.dtpcwayanad.com/destination/priyadarshini-hills-wayanad" title="Priyadarshini Hills">Priyadarshini Hills</a></h6><a target="_blank" href="https://www.dtpcwayanad.com/destination/priyadarshini-hills-wayanad" title="Priyadarshini Hills"><img alt=""  src="https://www.dtpcwayanad.com/uploads/picture_gallery/gallery_images/priyadarshini-tea-environs-wayanad-21-20230429102333297293.webp" width="300" ></a>', 9.655071684595004, 76.53276868947644, 2, "https://www.dtpcwayanad.com/images/pin-drop.png"],
 
-
-
   ['<h6><a target="_blank" href="https://www.dtpcwayanad.com/destination/seethadevi-lava-kusa-temple-wayanad" title="Seethadevi Lava Kusa Temple">Seethadevi Lava Kusa Temple</a></h6><a target="_blank" href="https://www.dtpcwayanad.com/destination/seethadevi-lava-kusa-temple-wayanad" title="Seethadevi Lava Kusa Temple"><img alt=""  src="https://www.dtpcwayanad.com/uploads/picture_gallery/gallery_images/seetha-lava-kusha-temple-wayanad-6-20230429103025483735.webp" width="300" ></a>', 11.792823, 76.168709, 2, "https://www.dtpcwayanad.com/images/pin-drop.png"],
-
-
 
   ['<h6><a target="_blank" href="https://www.dtpcwayanad.com/destination/thirunelli-temple-wayanad" title="Thirunelli Temple">Thirunelli Temple</a></h6><a target="_blank" href="https://www.dtpcwayanad.com/destination/thirunelli-temple-wayanad" title="Thirunelli Temple"><img alt=""  src="https://www.dtpcwayanad.com/uploads/picture_gallery/gallery_images/thirunelli-temple-wayanad-20-20230429103624709816.webp" width="300" ></a>', 11.911625, 75.995811, 2, "https://www.dtpcwayanad.com/images/pin-drop.png"],
 
-
-
   ['<h6><a target="_blank" href="https://www.dtpcwayanad.com/destination/valliyoorkavu-bhagavathy-temple-wayanad" title="Valliyoorkavu Bhagavathy Temple">Valliyoorkavu Bhagavathy Temple</a></h6><a target="_blank" href="https://www.dtpcwayanad.com/destination/valliyoorkavu-bhagavathy-temple-wayanad" title="Valliyoorkavu Bhagavathy Temple"><img alt=""  src="https://www.dtpcwayanad.com/uploads/picture_gallery/gallery_images/valliyurkavu-bhagavathy-temple-wayanad-8-20230429105520601012.webp" width="300" ></a>', 11.804457, 76.030023, 2, "https://www.dtpcwayanad.com/images/pin-drop.png"],
-
-
 
   ['<h6><a target="_blank" href="https://www.dtpcwayanad.com/destination/banasura-meenmutty-waterfalls-wayanad" title="Banasura Meenmutty Waterfalls">Banasura Meenmutty Waterfalls</a></h6><a target="_blank" href="https://www.dtpcwayanad.com/destination/banasura-meenmutty-waterfalls-wayanad" title="Banasura Meenmutty Waterfalls"><img alt=""  src="https://www.dtpcwayanad.com/uploads/picture_gallery/gallery_images/banasura-meenmutty-dsc05988-20230429170048643851.webp" width="300" ></a>', 11.677697, 75.935689, 2, "https://www.dtpcwayanad.com/images/pin-drop.png"],
 
-
-
   ['<h6><a target="_blank" href="https://www.dtpcwayanad.com/destination/chain-tree-lakkidi-wayanad" title="Chain Tree, Lakkidi">Chain Tree, Lakkidi</a></h6><a target="_blank" href="https://www.dtpcwayanad.com/destination/chain-tree-lakkidi-wayanad" title="Chain Tree, Lakkidi"><img alt=""  src="https://www.dtpcwayanad.com/uploads/picture_gallery/gallery_images/chain-tree-karinthandan-memorial-wayanad-3-20230429110919305727.webp" width="300" ></a>', 11.519108, 76.020890, 2, "https://www.dtpcwayanad.com/images/pin-drop.png"],
-
-
 
   ['<h6><a target="_blank" href="https://www.dtpcwayanad.com/destination/edakkal-caves-wayanad" title="Edakkal Caves">Edakkal Caves</a></h6><a target="_blank" href="https://www.dtpcwayanad.com/destination/edakkal-caves-wayanad" title="Edakkal Caves"><img alt=""  src="https://www.dtpcwayanad.com/uploads/picture_gallery/gallery_images/edakkal-caves-wayanad-trekking-20230429170000119738.webp" width="300" ></a>', 11.626856, 76.234287, 2, "https://www.dtpcwayanad.com/images/pin-drop.png"],
 
-
-
   ['<h6><a target="_blank" href="https://www.dtpcwayanad.com/destination/kanthanpara-waterfalls-wayanad" title="Kanthanpara Waterfalls">Kanthanpara Waterfalls</a></h6><a target="_blank" href="https://www.dtpcwayanad.com/destination/kanthanpara-waterfalls-wayanad" title="Kanthanpara Waterfalls"><img alt=""  src="https://www.dtpcwayanad.com/uploads/picture_gallery/gallery_images/kanthanpara-water-falls-1920x1080-20230429170550494762.webp" width="300" ></a>', 11.524134527182937, 76.1526895955301, 2, "https://www.dtpcwayanad.com/images/pin-drop.png"],
-
-
 
   ['<h6><a target="_blank" href="https://www.dtpcwayanad.com/destination/muthanga-wildlife-sanctuary-wayanad" title="Muthanga Wildlife Sanctuary">Muthanga Wildlife Sanctuary</a></h6><a target="_blank" href="https://www.dtpcwayanad.com/destination/muthanga-wildlife-sanctuary-wayanad" title="Muthanga Wildlife Sanctuary"><img alt=""  src="https://www.dtpcwayanad.com/uploads/picture_gallery/gallery_images/muthanga-wildlife-20230429170248643701.webp" width="300" ></a>', 11.671597, 76.368656, 2, "https://www.dtpcwayanad.com/images/pin-drop.png"],
 
-
-
   ['<h6><a target="_blank" href="https://www.dtpcwayanad.com/destination/pazhassi-smarakam-wayanad" title="Pazhassi Smarakam">Pazhassi Smarakam</a></h6><a target="_blank" href="https://www.dtpcwayanad.com/destination/pazhassi-smarakam-wayanad" title="Pazhassi Smarakam"><img alt=""  src="https://www.dtpcwayanad.com/uploads/picture_gallery/gallery_images/pazhassi-smarakam-1920x1080-20230503111015231294.webp" width="300" ></a>', 11.817826, 76.217964, 2, "https://www.dtpcwayanad.com/images/pin-drop.png"],
-
-
 
   ['<h6><a target="_blank" href="https://www.dtpcwayanad.com/destination/pookode-lake-wayanad" title="Pookode Lake">Pookode Lake</a></h6><a target="_blank" href="https://www.dtpcwayanad.com/destination/pookode-lake-wayanad" title="Pookode Lake"><img alt=""  src="https://www.dtpcwayanad.com/uploads/picture_gallery/gallery_images/pookode-lake-wayanad-3-20230429174142824511.webp" width="300" ></a>', 11.542470, 76.027224, 2, "https://www.dtpcwayanad.com/images/pin-drop.png"],
 
-
-
   ['<h6><a target="_blank" href="https://www.dtpcwayanad.com/destination/soochipara-waterfalls-wayanad" title="Soochipara Waterfalls">Soochipara Waterfalls</a></h6><a target="_blank" href="https://www.dtpcwayanad.com/destination/soochipara-waterfalls-wayanad" title="Soochipara Waterfalls"><img alt=""  src="https://www.dtpcwayanad.com/uploads/picture_gallery/gallery_images/soochipara-waterfalls-wayanad-14-20230429180555582741.webp" width="300" ></a>', 11.511654, 76.163737, 2, "https://www.dtpcwayanad.com/images/pin-drop.png"],
-
-
 
   ['<h6><a target="_blank" href="https://www.dtpcwayanad.com/destination/tholpetty-wildlife-sanctuary-wayanad" title="Tholpetty Wildlife Sanctuary">Tholpetty Wildlife Sanctuary</a></h6><a target="_blank" href="https://www.dtpcwayanad.com/destination/tholpetty-wildlife-sanctuary-wayanad" title="Tholpetty Wildlife Sanctuary"><img alt=""  src="https://www.dtpcwayanad.com/uploads/picture_gallery/gallery_images/tholpetty-wildlife-sanctuary-wayanad-8-20230429180457722888.webp" width="300" ></a>', 11.951629, 76.059502, 2, "https://www.dtpcwayanad.com/images/pin-drop.png"],
 
-
-
   ['<h6><a target="_blank" href="https://www.dtpcwayanad.com/destination/wayanad-heritage-museum-wayanad" title="Wayanad Heritage Museum">Wayanad Heritage Museum</a></h6><a target="_blank" href="https://www.dtpcwayanad.com/destination/wayanad-heritage-museum-wayanad" title="Wayanad Heritage Museum"><img alt=""  src="https://www.dtpcwayanad.com/uploads/picture_gallery/gallery_images/wayanad-heritage-museum-wayanad-32-20230429181434727269.webp" width="300" ></a>', 11.619335, 76.210626, 2, "https://www.dtpcwayanad.com/images/pin-drop.png"],
-
-
 
   ['<h6><a target="_blank" href="https://www.dtpcwayanad.com/destination/kuruva-island-wayanad" title="Kuruva Island">Kuruva Island</a></h6><a target="_blank" href="https://www.dtpcwayanad.com/destination/kuruva-island-wayanad" title="Kuruva Island"><img alt=""  src="https://www.dtpcwayanad.com/uploads/picture_gallery/gallery_images/kuruva-island-wayanad-24-20230429182424616090.webp" width="300" ></a>', 11.824576, 76.094040, 2, "https://www.dtpcwayanad.com/images/pin-drop.png"],
 
-
-
   ['<h6><a target="_blank" href="https://www.dtpcwayanad.com/destination/banasura-sagar-dam-wayanad" title="Banasura Sagar Dam">Banasura Sagar Dam</a></h6><a target="_blank" href="https://www.dtpcwayanad.com/destination/banasura-sagar-dam-wayanad" title="Banasura Sagar Dam"><img alt=""  src="https://www.dtpcwayanad.com/uploads/picture_gallery/gallery_images/banasura-sagar-dam-wayanad-43-20230429183921377875.webp" width="300" ></a>', 11.670692, 75.955571, 2, "https://www.dtpcwayanad.com/images/pin-drop.png"],
 
-
-
   ['<h6><a target="_blank" href="https://www.dtpcwayanad.com/destination/karapuzha-dam-wayanad" title="Karapuzha Dam">Karapuzha Dam</a></h6><a target="_blank" href="https://www.dtpcwayanad.com/destination/karapuzha-dam-wayanad" title="Karapuzha Dam"><img alt=""  src="https://www.dtpcwayanad.com/uploads/picture_gallery/gallery_images/karappuzha-dam-wayanad-6-20230429185056480109.webp" width="300" ></a>', 11.618487, 76.167956, 2, "https://www.dtpcwayanad.com/images/pin-drop.png"],
-
-
-
-
 ];
 
-//var iconURLPrefix = 'https://www.dtpcwayanad.com/images/';
 var map = new google.maps.Map(document.getElementById('map'), {
   zoom: 10,
-  // disable the default User Interface
-  disableDefaultUI: true,
-  // add back fullscreen, streetview, zoom
+  disableDefaultUI: false,
   zoomControl: true,
   streetViewControl: true,
   fullscreenControl: true,
+  mapTypeControl: true,
   center: new google.maps.LatLng(11.6271608969693, 76.08851019534403),
-  //	mapTypeId: google.maps.MapTypeId.ROADMAP,
-  mapId: 'f03033acde18bc0d'
+  mapId: 'f03033acde18bc0d',
+  styles: [
+    {
+      "featureType": "all",
+      "elementType": "geometry",
+      "stylers": [{"color": "#242f3e"}]
+    },
+    {
+      "featureType": "all",
+      "elementType": "labels.text.stroke",
+      "stylers": [{"lightness": -80}]
+    },
+    {
+      "featureType": "administrative",
+      "elementType": "labels.text.fill",
+      "stylers": [{"color": "#ffffff"}]
+    },
+    {
+      "featureType": "administrative.locality",
+      "elementType": "labels.text.fill",
+      "stylers": [{"color": "#ffffff"}]
+    },
+    {
+      "featureType": "poi",
+      "elementType": "labels.text.fill",
+      "stylers": [{"color": "#ffffff"}]
+    },
+    {
+      "featureType": "poi.park",
+      "elementType": "geometry",
+      "stylers": [{"color": "#263c3f"}]
+    },
+    {
+      "featureType": "poi.park",
+      "elementType": "labels.text.fill",
+      "stylers": [{"color": "#ffffff"}]
+    },
+    {
+      "featureType": "road",
+      "elementType": "geometry",
+      "stylers": [{"color": "#38414e"}]
+    },
+    {
+      "featureType": "road",
+      "elementType": "geometry.stroke",
+      "stylers": [{"color": "#212a37"}]
+    },
+    {
+      "featureType": "road",
+      "elementType": "labels.text.fill",
+      "stylers": [{"color": "#ffffff"}]
+    },
+    {
+      "featureType": "road.highway",
+      "elementType": "geometry",
+      "stylers": [{"color": "#746855"}]
+    },
+    {
+      "featureType": "road.highway",
+      "elementType": "geometry.stroke",
+      "stylers": [{"color": "#1f2835"}]
+    },
+    {
+      "featureType": "road.highway",
+      "elementType": "labels.text.fill",
+      "stylers": [{"color": "#ffffff"}]
+    },
+    {
+      "featureType": "water",
+      "elementType": "geometry",
+      "stylers": [{"color": "#17263c"}]
+    },
+    {
+      "featureType": "water",
+      "elementType": "labels.text.fill",
+      "stylers": [{"color": "#ffffff"}]
+    },
+    {
+      "featureType": "water",
+      "elementType": "labels.text.stroke",
+      "stylers": [{"lightness": -20}]
+    }
+  ]
 });
+
 var infowindow = new google.maps.InfoWindow();
-var marker, i;
+var markers = [];
+var directionsService = new google.maps.DirectionsService();
+var directionsRenderer = new google.maps.DirectionsRenderer({
+  map: map,
+  suppressMarkers: true,
+  polylineOptions: {
+    strokeColor: '#4285F4',
+    strokeWeight: 4,
+    strokeOpacity: 0.8
+  }
+});
+
+// Find BW Stays location
+var bwLocation = locations.find(loc => loc[0].includes('Black & White'));
+var bwPosition = bwLocation ? new google.maps.LatLng(bwLocation[1], bwLocation[2]) : null;
+
+// Add markers and routes
 for (i = 0; i < locations.length; i++) {
-  marker = new google.maps.Marker({
-    position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-    icon: locations[i][4],
-    map: map
+  var position = new google.maps.LatLng(locations[i][1], locations[i][2]);
+  var isBW = locations[i][0].includes('Black & White');
+  
+  var marker = new google.maps.Marker({
+    position: position,
+    icon: {
+      url: locations[i][4],
+      scaledSize: new google.maps.Size(40, 40),
+      labelOrigin: new google.maps.Point(20, -10)
+    },
+    map: map,
+    title: locations[i][0].match(/<h6>.*?<\/h6>/)[0].replace(/<[^>]*>/g, ''),
+    label: {
+      text: locations[i][0].match(/<h6>.*?<\/h6>/)[0].replace(/<[^>]*>/g, ''),
+      color: "#ffffff",
+      fontSize: "12px",
+      fontWeight: "bold"
+    }
   });
-  google.maps.event.addListener(marker, 'click', (function (marker, i) {
-    return function () {
+  
+  markers.push(marker);
+  
+  // Add click listener for info window
+  google.maps.event.addListener(marker, 'click', (function(marker, i) {
+    return function() {
       infowindow.setContent(locations[i][0]);
       infowindow.open(map, marker);
     }
   })(marker, i));
+  
+  // Draw route from BW Stays to this location if it's not BW Stays
+  if (bwPosition && !isBW) {
+    var request = {
+      origin: bwPosition,
+      destination: position,
+      travelMode: google.maps.TravelMode.DRIVING
+    };
+    
+    directionsService.route(request, function(result, status) {
+      if (status === 'OK') {
+        directionsRenderer.setDirections(result);
+      }
+    });
+  }
 }
+
+// Fit map to show all markers with padding
+var bounds = new google.maps.LatLngBounds();
+markers.forEach(function(marker) {
+  bounds.extend(marker.getPosition());
+});
+map.fitBounds(bounds, {
+  padding: {
+    top: 50,
+    right: 50,
+    bottom: 50,
+    left: 50
+  }
+});
 
 function getData() {
   document.frm.submit();
