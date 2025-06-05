b164331f810cdd40ade070456496a011a027c0fc
bwstays <bwstays@gmail.com>
2025-06-05 17:35:11 +0530

    updated

---
+++ js/kalpetta_all_food_locations.js
@@ -0,0 +1,183 @@
+
+
+var foodations = [
+
+
+  ['<h6><a target="_blank" href="https://www.bwstays.com" title="Black & White">Black & White</a></h6><a target="_blank" href="https://www.dtpcwayanad.com/destination/chembra-peak-wayanad" title="BW"><img alt=""  src="https://www.dtpcwayanad.com/uploads/picture_gallery/gallery_images/chembra-peak-20230429161812306996.webp" width="300" ></a>', 11.605943, 76.083429, 2, "assets/img/logo/bw.png"],
+
+
+['<h6><a target="_blank" href="Muzwalla Resturant" title="Muzwalla Resturant">Muzwalla Resturant</a></h6><a target="_blank" href="https://Muzwalla Resturantd" title="Muzwalla Resturant"><img alt=""  src="assets/img/heritage/Muzwalla Resturant.webp" width="300" ></a>', 11.6963, 76.1411, 2, "assets/img/logo/restaurant.png"],
+
+
+['<h6><a target="_blank" href="Hotel Paradise" title="Majestic Bakes">Majestic Bakes</a></h6><a target="_blank" href="https://Muzwalla Resturantd" title="Muzwalla Resturant"><img alt=""  src="assets/img/heritage/Muzwalla Resturant.webp" width="300" ></a>', 11.604323109796422, 76.08630892473452, 2, "assets/img/logo/restaurant.png"],
+
+['<h6><a target="_blank" href="Krishna Bhavan Pure Veg" title="Majestic Bakes">Krishna Bhavan Pure Veg</a></h6><a target="_blank" href="https://Muzwalla Resturantd" title="Muzwalla Resturant"><img alt=""  src="assets/img/heritage/Muzwalla Resturant.webp" width="300" ></a>', 11.60585860731025, 76.08680326940188, 2, "assets/img/logo/restaurant.png"],
+
+['<h6><a target="_blank" href="Majestic Bakes and Resturants" title="Majestic Bakes">Majestic Bakes and Resturants</a></h6><a target="_blank" href="https://Muzwalla Resturantd" title="Muzwalla Resturant"><img alt=""  src="assets/img/heritage/Muzwalla Resturant.webp" width="300" ></a>', 11.604780705721431, 76.08324548447946, 2, "assets/img/logo/restaurant.png"],
+
+['<h6><a target="_blank" href="Nesto Hyper market " title="Majestic Bakes">Nesto Hyper market</a></h6><a target="_blank" href="https://Muzwalla Resturantd" title="Muzwalla Resturant"><img alt=""  src="assets/img/heritage/Muzwalla Resturant.webp" width="300" ></a>', 11.60340794748994, 76.08304528423753, 2, "assets/img/logo/restaurant.png"],
+
+['<h6><a target="_blank" href="Chatiyum Chorum " title="Chatiyum Chorum">Chatiyum Chorum</a></h6><a target="_blank" href="https://Muzwalla Resturantd" title="Muzwalla Resturant"><img alt=""  src="assets/img/heritage/Muzwalla Resturant.webp" width="300" ></a>', 11.605200936482868, 76.08393188530898, 2, "assets/img/logo/restaurant.png"],
+
+
+['<h6><a target="_blank" href="The Bungalow " title="The Bungalow">The Bungalow</a></h6><a target="_blank" href="https://Muzwalla Resturantd" title="Muzwalla Resturant"><img alt=""  src="assets/img/heritage/Muzwalla Resturant.webp" width="300" ></a>', 11.606377579247457, 76.08390328527442, 2, "assets/img/logo/restaurant.png"],
+
+
+['<h6><a target="_blank" href="Affas Pure Vegiterian " title="Affas Pure Vegiterian">Affas Pure Vegiterian</a></h6><a target="_blank" href="https://Muzwalla Resturantd" title="Muzwalla Resturant"><img alt=""  src="assets/img/heritage/Muzwalla Resturant.webp" width="300" ></a>', 11.607133989832576, 76.08324548447946, 2, "assets/img/logo/restaurant.png"],
+
+
+
+
+['<h6><a target="_blank" href="Jallels Kitchen " title="Jallels Kitchen">Jallels Kitchen</a></h6><a target="_blank" href="https://Muzwalla Resturantd" title="Muzwalla Resturant"><img alt=""  src="assets/img/heritage/Muzwalla Resturant.webp" width="300" ></a>', 11.610667798353557, 76.0833430886246, 2, "assets/img/logo/cafe.png"],
+
+['<h6><a target="_blank" href="Alibaba and 41 Dishes" title="Alibaba and 41 Dishes">Alibaba and 41 Dishes</a></h6><a target="_blank" href="https://Muzwalla Resturantd" title="Muzwalla Resturant"><img alt=""  src="assets/img/heritage/Muzwalla Resturant.webp" width="300" ></a>', 11.610318686152537, 76.08084825487074, 2, "assets/img/logo/restaurant.png"],
+
+['<h6><a target="_blank" href="Dosa Cafe " title="Dosa Cafe ">Dosa Cafe </a></h6><a target="_blank" href="https://Muzwalla Resturantd" title="Muzwalla Resturant"><img alt=""  src="assets/img/heritage/Muzwalla Resturant.webp" width="300" ></a>', 11.61319990659965, 76.08091551813482, 2, "assets/img/logo/cafe.png"],
+
+['<h6><a target="_blank" href="New Form Resturant " title="New Form Resturant">New Form Resturant</a></h6><a target="_blank" href="https://Muzwalla Resturantd" title="Muzwalla Resturant"><img alt=""  src="assets/img/heritage/Muzwalla Resturant.webp" width="300" ></a>', 11.614539422188063, 76.08262490982439, 2, "assets/img/logo/restaurant.png"],
+
+['<h6><a target="_blank" href="Holiday Village " title="Holiday Village">Holiday Village</a></h6><a target="_blank" href="https://Muzwalla Resturantd" title="Muzwalla Resturant"><img alt=""  src="assets/img/heritage/Muzwalla Resturant.webp" width="300" ></a>', 11.616672058724552, 76.08291280737211, 2, "assets/img/logo/restaurant.png"],
+
+
+['<h6><a target="_blank" href="Beyco Wayanad " title="Beyco Wayanad ">Beyco Wayanad </a></h6><a target="_blank" href="https://Muzwalla Resturantd" title="Muzwalla Resturant"><img alt=""  src="assets/img/heritage/Muzwalla Resturant.webp" width="300" ></a>', 11.612653523348222, 76.08800499522312, 2, "assets/img/logo/restaurant.png"],
+
+
+['<h6><a target="_blank" href="Rayan Resturant " title="Rayan Resturant ">Rayan Resturant </a></h6><a target="_blank" href="https://Muzwalla Resturantd" title="Muzwalla Resturant"><img alt=""  src="assets/img/heritage/Muzwalla Resturant.webp" width="300" ></a>', 11.619758043374217, 76.08496651902011, 2, "assets/img/logo/restaurant.png"],
+
+
+['<h6><a target="_blank" href="Kabab Shack" title="Kabab Shack">Kabab Shack</a></h6><a target="_blank" href="https://Muzwalla Resturantd" title="Muzwalla Resturant"><img alt=""  src="assets/img/heritage/Muzwalla Resturant.webp" width="300" ></a>', 11.6203363540984, 76.08884269768355, 2, "assets/img/logo/restaurant.png"],
+
+
+['<h6><a target="_blank" href="1980 Nostalgia " title="1980 Nostalgia">1980 Nostalgia</a></h6><a target="_blank" href="https://Muzwalla Resturantd" title="Muzwalla Resturant"><img alt=""  src="assets/img/heritage/Muzwalla Resturant.webp" width="300" ></a>', 11.620851803949527, 76.08899671802779, 2, "assets/img/logo/restaurant.png"],
+
+
+['<h6><a target="_blank" href="Oshin Resturant " title="Oshin Resturant">Oshin Resturant</a></h6><a target="_blank" href="https://Muzwalla Resturantd" title="Muzwalla Resturant"><img alt=""  src="assets/img/heritage/Muzwalla Resturant.webp" width="300" ></a>', 11.626228731134612, 76.087809496246, 2, "assets/img/logo/restaurant.png"],
+
+
+['<h6><a target="_blank" href="Bevco Beverages " title="Bevco Beverages">Bevco Beverages</a></h6><a target="_blank" href="https://Muzwalla Resturantd" title="Muzwalla Resturant"><img alt=""  src="assets/img/heritage/Muzwalla Resturant.webp" width="300" ></a>', 11.624334351501458, 76.09030848070677, 2, "assets/img/logo/restaurant.png"],
+
+['<h6><a target="_blank" href="Ramavilas Vegiterain" title="Ramavilas Vegiterain">Ramavilas Vegiterain</a></h6><a target="_blank" href="https://Muzwalla Resturantd" title="Muzwalla Resturant"><img alt=""  src="assets/img/heritage/Muzwalla Resturant.webp" width="300" ></a>', 11.610380054042501, 76.08310942611584, 2, "assets/img/logo/restaurant.png"],
+
+
+['<h6><a target="_blank" href="Ruchi Pura " title="Ruchi Pura ">Ruchi Pura </a></h6><a target="_blank" href="https://Muzwalla Resturantd" title="Muzwalla Resturant"><img alt=""  src="assets/img/heritage/Muzwalla Resturant.webp" width="300" ></a>', 11.605751134803608, 76.08368257503619, 2, "assets/img/logo/restaurant.png"],
+
+['<h6><a target="_blank" href="Hotel Paradise" title="Hotel Paradise">Hotel Paradise</a></h6><a target="_blank" href="https://Muzwalla Resturantd" title="Muzwalla Resturant"><img alt=""  src="assets/img/heritage/Muzwalla Resturant.webp" width="300" ></a>', 11.60176526999694, 76.08164659461592, 2, "assets/img/logo/restaurant.png"],
+
+['<h6><a target="_blank" href="Ruchi Pura " title="Muzwalla Resturant ">Muzwalla Resturant</a></h6><a target="_blank" href="https://Muzwalla Resturantd" title="Muzwalla Resturant"><img alt=""  src="assets/img/heritage/Muzwalla Resturant.webp" width="300" ></a>', 11.600690382919279, 76.08111456843432, 2, "assets/img/logo/restaurant.png"],
+
+['<h6><a target="_blank" href="Fruit Bae" title="Fruit Bae">Fruit Bae</a></h6><a target="_blank" href="https://Muzwalla Resturantd" title="Muzwalla Resturant"><img alt=""  src="assets/img/heritage/Muzwalla Resturant.webp" width="300" ></a>', 11.60119739560447, 76.08164781022073, 2, "assets/img/logo/restaurant.png"],
+
+['<h6><a target="_blank" href="Chicking" title="Chicking">Chicking</a></h6><a target="_blank" href="https://Muzwalla Resturantd" title="Muzwalla Resturant"><img alt=""  src="assets/img/heritage/Muzwalla Resturant.webp" width="300" ></a>', 11.603008465130017, 76.08304000088835, 2, "assets/img/logo/restaurant.png"],
+
+
+['<h6><a target="_blank" href="Cafe Cocktail" title="Cafe Cocktail">Cafe Cocktail</a></h6><a target="_blank" href="https://Muzwalla Resturantd" title="Muzwalla Resturant"><img alt=""  src="assets/img/heritage/Muzwalla Resturant.webp" width="300" ></a>', 11.604506794638315, 76.08376943606282, 2, "assets/img/logo/cafe.png"],
+
+
+['<h6><a target="_blank" href="Cup Coffee" title="Cup Coffee">Cup Coffee</a></h6><a target="_blank" href="https://Muzwalla Resturantd" title="Muzwalla Resturant"><img alt=""  src="assets/img/heritage/Muzwalla Resturant.webp" width="300" ></a>', 11.60659514471941, 76.08388093697519, 2, "assets/img/logo/cafe.png"],
+
+
+['<h6><a target="_blank" href="Cup Coffee" title="Cup Coffee">Cup Coffee</a></h6><a target="_blank" href="https://Muzwalla Resturantd" title="Muzwalla Resturant"><img alt=""  src="assets/img/heritage/Muzwalla Resturant.webp" width="300" ></a>', 11.614914415984193, 76.08302861096685, 2, "assets/img/logo/cafe.png"],
+
+
+['<h6><a target="_blank" href="New Hotel" title="New Hotel">New Hotel</a></h6><a target="_blank" href="https://Muzwalla Resturantd" title="Muzwalla Resturant"><img alt=""  src="assets/img/heritage/Muzwalla Resturant.webp" width="300" ></a>', 11.607452677265156, 76.08362059821074, 2, "assets/img/logo/restaurant.png"],
+
+
+
+['<h6><a target="_blank" href="Chill Out" title="Chill Out">Chill Out</a></h6><a target="_blank" href="https://Muzwalla Resturantd" title="Muzwalla Resturant"><img alt=""  src="assets/img/heritage/Muzwalla Resturant.webp" width="300" ></a>', 11.608996367231134, 76.08346178431843, 2, "assets/img/logo/cafe.png"],
+
+
+['<h6><a target="_blank" href="Vanita Mess" title="Vanita Mess">Vanita Mess</a></h6><a target="_blank" href="https://Muzwalla Resturantd" title="Muzwalla Resturant"><img alt=""  src="assets/img/heritage/Muzwalla Resturant.webp" width="300" ></a>', 11.609012788111084, 76.0832646419536, 2, "assets/img/logo/restaurant.png"],
+
+
+['<h6><a target="_blank" href="Walnuts cake shop" title="Walnuts cake shop">Walnuts cake shop</a></h6><a target="_blank" href="https://Muzwalla Resturantd" title="Muzwalla Resturant"><img alt=""  src="assets/img/heritage/Muzwalla Resturant.webp" width="300" ></a>', 11.614855354258184, 76.08281021303468, 2, "assets/img/logo/cafe.png"],
+
+
+
+
+['<h6><a target="_blank" href="Cafe 41 Kalpetta" title="Cafe 41 Kalpetta">Cafe 41 Kalpetta</a></h6><a target="_blank" href="https://Muzwalla Resturantd" title="Muzwalla Resturant"><img alt=""  src="assets/img/heritage/Muzwalla Resturant.webp" width="300" ></a>', 11.610153825099772, 76.08097305952563, 2, "assets/img/logo/cafe.png"],
+
+
+['<h6><a target="_blank" href="Olan Resturant" title="Olan Resturant">Olan Resturant</a></h6><a target="_blank" href="https://Muzwalla Resturantd" title="Muzwalla Resturant"><img alt=""  src="assets/img/heritage/Muzwalla Resturant.webp" width="300" ></a>', 11.644692054539417, 76.11396083068944, 2, "assets/img/logo/restaurant.png"],
+
+
+['<h6><a target="_blank" href="Touffequqee Resturant" title="Touffequqee Resturant">Touffequqee Resturant</a></h6><a target="_blank" href="https://Muzwalla Resturantd" title="Muzwalla Resturant"><img alt=""  src="assets/img/heritage/Muzwalla Resturant.webp" width="300" ></a>', 11.615868291399686, 76.08321320185325, 2, "assets/img/logo/restaurant.png"],
+
+['<h6><a target="_blank" href="Hotel Pankaj" title="Hotel Pankaj">Hotel Pankaj</a></h6><a target="_blank" href="https://Muzwalla Resturantd" title="Muzwalla Resturant"><img alt=""  src="assets/img/heritage/Muzwalla Resturant.webp" width="300" ></a>', 11.609547800012608, 76.08195152328292, 2, "assets/img/logo/restaurant.png"],
+
+
+['<h6><a target="_blank" href="Night Tattu Kada" title="Night Tattu Kada">Night Tattu Kada</a></h6><a target="_blank" href="https://Muzwalla Resturantd" title="Muzwalla Resturant"><img alt=""  src="assets/img/heritage/Muzwalla Resturant.webp" width="300" ></a>', 11.604806010573219, 76.08376805542096, 2, "assets/img/logo/restaurant.png"],
+
+
+
+['<h6><a target="_blank" href="Maracana Resturants" title="Maracana Resturants">Maracana Resturants</a></h6><a target="_blank" href="https://Muzwalla Resturantd" title="Muzwalla Resturant"><img alt=""  src="assets/img/heritage/Muzwalla Resturant.webp" width="300" ></a>', 11.600071240199552, 76.0812581584653, 2, "assets/img/logo/restaurant.png"],
+
+['<h6><a target="_blank" href="Casa Mia 2.1" title="Casa Mia 2.1">Casa Mia 2.1</a></h6><a target="_blank" href="https://Muzwalla Resturantd" title="Muzwalla Resturant"><img alt=""  src="assets/img/heritage/Muzwalla Resturant.webp" width="300" ></a>', 11.608590691762718, 76.08721315614605, 2, "assets/img/logo/restaurant.png"],
+
+['<h6><a target="_blank" href="The Coffee Shop" title="The Coffee Shop">The Coffee Shop</a></h6><a target="_blank" href="https://Muzwalla Resturantd" title="Muzwalla Resturant"><img alt=""  src="assets/img/heritage/Muzwalla Resturant.webp" width="300" ></a>', 11.617154317507989, 76.06854264615934, 2, "assets/img/logo/restaurant.png"],
+
+
+['<h6><a target="_blank" href="Ramavilas Chundale" title="Ramavilas Chundale">Ramavilas Chundale</a></h6><a target="_blank" href="https://Muzwalla Resturantd" title="Muzwalla Resturant"><img alt=""  src="assets/img/heritage/Muzwalla Resturant.webp" width="300" ></a>', 11.5738608278821, 76.05500967074094, 2, "assets/img/logo/restaurant.png"],
+
+
+
+
+
+  ];
+
+
+//var iconURLPrefix = 'https://www.dtpcwayanad.com/images/';
+var map = new google.maps.Map(document.getElementById('mapall'), {
+  zoom: 9.999,
+  // disable the default User Interface
+  disableDefaultUI: true,
+  // add back fullscreen, streetview, zoom
+  zoomControl: true,
+  streetViewControl: true,
+  fullscreenControl: true,
+  //panamaram center
+  center: new google.maps.LatLng(11.606592761660165, 76.08350199577485),
+  //	mapTypeId: google.maps.MapTypeId.ROADMAP,
+  mapId: 'f03033acde18bc0d'
+});
+var infowindow = new google.maps.InfoWindow();
+var  i;
+var markers = [];
+
+
+
+for (i = 0; i < foodations.length; i++) {
+
+ new google.maps.DirectionsService().route({
+      origin: new google.maps.LatLng( 11.606592761660165, 76.08350199577485), // origin is bw stay
+      destination: new google.maps.LatLng(foodations[i][1], foodations[i][2]),
+      travelMode: google.maps.TravelMode.DRIVING,
+    }, (response, status) => {
+        if (status === "OK") {
+          new google.maps.DirectionsRenderer({
+						  map: map,
+						  suppressMarkers: true,
+						  polylineOptions: {
+							strokeColor: '#4285F4',
+							strokeWeight: 4,
+							strokeOpacity: 0.8
+						  }
+						}).setDirections(response);
+        }
+  })
+
+  marker = new google.maps.Marker({
+    position: new google.maps.LatLng(foodations[i][1], foodations[i][2]),
+    icon: foodations[i][4],
+    map: map
+  });
+
+
+  google.maps.event.addListener(marker, 'click', (function (marker, i) {
+    return function () {
+      infowindow.setContent(foodations[i][0]);
+      infowindow.open(map, marker);
+    }
+  })(marker, i));
+
+}
+
