6bd2945532bfce0ae61e3cb185bd4a76946b50aa
bwstays <bwstays@gmail.com>
2025-06-24 12:29:56 +0530

    updated

--- js/bwmaps.js
+++ js/bwmaps.js
@@ -3,6 +3,11 @@ var iconURLPrefix = '';
 
 var bwlocations = [['<h6><a target="_blank" href="https://bwstays.github.io/bwstays" title="Black and White Stays">Black and White Stays</a></h6><a target="_blank" href="https://bwstays.github.io/bwstays" title="Black and White Stays"><img alt="Black and White Stays Wayanad" alt="Black and White Stays Wayanad"  src='+iconURLPrefix+'"assets/img/logo/pin-drop.png" width="300" ></a>', 11.542470, 76.027224, 2, iconURLPrefix+"assets/img/logo/bw.png"],];
 
+//  const { Map } = await google.maps.importLibrary("maps");
+
+//  const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary("marker");
+
+
 var map = new google.maps.Map(document.getElementById('map'), {
   zoom: 10,
   // disable the default User Interface
@@ -17,6 +22,25 @@ var map = new google.maps.Map(document.getElementById('map'), {
 });
 var infowindow = new google.maps.InfoWindow();
 var marker, i;
+
+/*
+https://jsfiddle.net/gh/get/library/pure/googlemaps/js-samples/tree/master/dist/samples/advanced-markers-html/jsfiddle
+ // Marker with a custom PinElement.
+  const pinScaled = new PinElement({
+    scale: 1.5,
+  });
+
+// Basic marker with title.
+  const markerViewWithText = new AdvancedMarkerElement({
+    map,
+    position: { lat: bwlocations[i][1], lng: bwlocations[i][2] },
+    content: pinScaled.element,
+  });
+  */
+
+
+
+
 for (i = 0; i < bwlocations.length; i++) {
   marker = new google.maps.Marker({
     position: new google.maps.LatLng(bwlocations[i][1], bwlocations[i][2]),
