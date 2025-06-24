6e932752f6e25f8dbaecfc5de09bbeca0b58c0fb
bwstays <bwstays@gmail.com>
2025-06-24 12:10:54 +0530

    uploaded

--- js/bwmaps.js
+++ js/bwmaps.js
@@ -2,8 +2,7 @@
 var iconURLPrefix = '';
 
 var bwlocations = [['<h6><a target="_blank" href="https://bwstays.github.io/bwstays" title="Black and White Stays">Black and White Stays</a></h6><a target="_blank" href="https://bwstays.github.io/bwstays" title="Black and White Stays"><img alt="Black and White Stays Wayanad" alt="Black and White Stays Wayanad"  src='+iconURLPrefix+'"assets/img/logo/pin-drop.png" width="300" ></a>', 11.542470, 76.027224, 2, iconURLPrefix+"assets/img/logo/bw.png"],];
-
-const {AdvancedMarkerElement} = await google.maps.importLibrary("marker");
+//const {AdvancedMarkerElement} = await google.maps.importLibrary("marker");
 
 var map = new google.maps.Map(document.getElementById('map'), {
   zoom: 10,
@@ -20,12 +19,10 @@ var map = new google.maps.Map(document.getElementById('map'), {
 var infowindow = new google.maps.InfoWindow();
 var marker, i;
 for (i = 0; i < bwlocations.length; i++) {
-
-  marker = new  AdvancedMarkerElement({
-	  map: map,
+  marker = new google.maps.Marker({
     position: new google.maps.LatLng(bwlocations[i][1], bwlocations[i][2]),
-    icon: bwlocations[i][4]
-
+    icon: bwlocations[i][4],
+    map: map
   });
   google.maps.event.addListener(marker, 'click', (function (marker, i) {
     return function () {
