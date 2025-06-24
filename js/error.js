0b6045f185bc6701241e2a38206a178a1bfc4dcc
bwstays <bwstays@gmail.com>
2025-06-24 12:58:28 +0530

    updated

---
+++ js/error.js
@@ -0,0 +1,33 @@
+const filesToCache = [
+  '/',
+  'error.html'
+];
+const staticCacheName = 'pages-cache-v1';
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(staticCacheName).then(cache => {
+      return cache.addAll(filesToCache);
+    });
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      // found a cached resource
+      if (response) {
+        return response;
+      }
+
+      // request the non-cached resource
+      return fetch(event.request).then(response => {
+
+        // fetch request returned 404, serve custom 404 page
+        if (response.status === 404) {
+          return caches.match('error.html');
+        }
+      });
+    });
+  );
+});
