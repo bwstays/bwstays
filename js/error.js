2e700a0d08de8019cd2ae73b036226fc8827697c
bwstays <bwstays@gmail.com>
2025-06-24 13:12:59 +0530

    updated

--- js/error.js
+++ js/error.js
@@ -7,6 +7,7 @@ const staticCacheName = 'pages-cache-v1';
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(staticCacheName).then(cache => {
+		alert(0)
       return cache.addAll(filesToCache);
     })
   );
@@ -15,6 +16,7 @@ self.addEventListener('install', event => {
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
+		alert(1)
       // found a cached resource
       if (response) {
         return response;
@@ -23,6 +25,7 @@ self.addEventListener('fetch', event => {
       // request the non-cached resource
       return fetch(event.request).then(response => {
 
+alert(2)
         // fetch request returned 404, serve custom 404 page
         if (response.status === 404) {
           return caches.match('https://bwstays.github.io/bwstays/error.html');
