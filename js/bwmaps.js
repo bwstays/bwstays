dbddf9d7c3b9b042caad2e0b684e215e7b0abffc
bwstays <bwstays@gmail.com>
2025-06-24 12:13:36 +0530

    updated

--- js/bwmaps.js
+++ js/bwmaps.js
@@ -2,7 +2,6 @@
 var iconURLPrefix = '';
 
 var bwlocations = [['<h6><a target="_blank" href="https://bwstays.github.io/bwstays" title="Black and White Stays">Black and White Stays</a></h6><a target="_blank" href="https://bwstays.github.io/bwstays" title="Black and White Stays"><img alt="Black and White Stays Wayanad" alt="Black and White Stays Wayanad"  src='+iconURLPrefix+'"assets/img/logo/pin-drop.png" width="300" ></a>', 11.542470, 76.027224, 2, iconURLPrefix+"assets/img/logo/bw.png"],];
-//const {AdvancedMarkerElement} = await google.maps.importLibrary("marker");
 
 var map = new google.maps.Map(document.getElementById('map'), {
   zoom: 10,
