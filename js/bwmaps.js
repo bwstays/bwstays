a3caa264ccfde8b54a0f6045f0aae0d2a8e50b6f
bwstays <bwstays@gmail.com>
2025-07-23 21:05:48 +0530

    Update bwmaps.js

--- js/bwmaps.js
+++ js/bwmaps.js
@@ -41,10 +41,11 @@ https://jsfiddle.net/gh/get/library/pure/googlemaps/js-samples/tree/master/dist/
 
 
 
+
 for (i = 0; i < bwlocations.length; i++) {
   marker = new google.maps.Marker({
     position: new google.maps.LatLng(bwlocations[i][1], bwlocations[i][2]),
-    icon: bwlocations[i][4],
+    icon:  {url: bwlocations[i][4],scaledSize: new google.maps.Size(50, 50), origin: new google.maps.Point(0,0),anchor: new google.maps.Point(0, 0) },
     map: map
   });
   google.maps.event.addListener(marker, 'click', (function (marker, i) {
