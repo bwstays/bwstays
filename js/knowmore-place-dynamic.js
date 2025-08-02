9d8dfb4cb7402091dcd80e9c27f550cc5056954c
bwstays <bwstays@gmail.com>
2025-08-03 00:00:45 +0530

    Update knowmore-place-dynamic.js

--- js/knowmore-place-dynamic.js
+++ js/knowmore-place-dynamic.js
@@ -250,7 +250,7 @@ $("#nearby-places h2").text("Location Nearby " + currentItemData.name);
   rightColumnHead.className = 'border-bottom border-primary text-center mb-4';
   rightColumnHead.style ='width:100%;margin:0 auto';
   rightColumnHead.innerHTML = `
-      <h5 class="text-white">${currentItemData.type} Places</h5>  `;
+      <h5 class="text-white">${currentItemData.type} Placesssss</h5>  `;
 
   // right pane content
   const rightColumn = document.createElement('div');
