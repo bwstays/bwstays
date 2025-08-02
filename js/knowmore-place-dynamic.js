28087ef88b0bf1ee1ae6838eb4f13b70052165b5
Aditya Mishra <mishraaditya3456@gmail.com>
2025-08-02 20:11:55 +0530

    testing

--- js/knowmore-place-dynamic.js
+++ js/knowmore-place-dynamic.js
@@ -125,6 +125,8 @@ rightArrow.addEventListener('mouseleave', () => {
   rightArrow.style.transform = 'translateY(-50%) scale(1)';
 });
 
+console.log("knowmore-place-dynamic.js loaded");
+
   // Get the first item from currentItem array
   const currentItemData = currentItem[0];
 
