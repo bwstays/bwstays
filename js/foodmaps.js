9fe1a598499967924e24f54c3f405087293df0b1
bwstays <bwstays@gmail.com>
2025-08-07 08:50:54 +0530

    Update foodmaps.js

--- js/foodmaps.js
+++ js/foodmaps.js
@@ -3,7 +3,7 @@ var iconURLPrefix = 'https://www.bwstays.com/';
 
 var bwlocations = [[11.542841751488135, 76.02595022698385]];
 
-var map = new google.maps.Map(document.getElementById('foodmap'), {
+var map1 = new google.maps.Map(document.getElementById('foodmap'), {
   zoom: 10,
   // disable the default User Interface
   disableDefaultUI: true,
@@ -17,11 +17,12 @@ var map = new google.maps.Map(document.getElementById('foodmap'), {
 });
 var infowindow = new google.maps.InfoWindow();
 var marker, i;
+performNearbySearch(map1,new google.maps.LatLng('11.542841751488135', '76.02595022698385'))
 for (i = 0; i < bwlocations.length; i++) {
   marker = new google.maps.Marker({
     position: new google.maps.LatLng('11.542841751488135', '76.02595022698385'),
     icon: 'https://maps.google.com/mapfiles/ms/icons/purple-dot.png',
-    map: map
+    map: map1
   });
   google.maps.event.addListener(marker, 'click', (function (marker, i) {
     return function () {
@@ -29,4 +30,29 @@ for (i = 0; i < bwlocations.length; i++) {
       infowindow.open(map, marker);
     }
   })(marker, i));
+}
+
+function performNearbySearch(map1, center) {
+  const service = new google.maps.places.PlacesService(map);
+  const request = {
+    location: center,
+    radius: 5000, // Search within a 5km radius
+    types: ['restaurant'],
+  };
+
+  service.nearbySearch(request, (results, status) => {
+    if (status === google.maps.places.PlacesServiceStatus.OK && results) {
+      for (let i = 0; i < results.length; i++) {
+        createMarker(results[i], map1);
+      }
+    }
+  });
+}
+
+function createMarker(place, map1) {
+  new google.maps.Marker({
+    map: map1,
+    position: place.geometry.location,
+    title: place.name,
+  });
 }
