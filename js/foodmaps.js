3a74f5f43475765c51fe768261137e1898c69b57
bwstays <bwstays@gmail.com>
2025-08-07 12:37:48 +0530

    Update foodmaps.js

--- js/foodmaps.js
+++ js/foodmaps.js
@@ -31,7 +31,6 @@ const service = new google.maps.places.PlacesService(map1);
 				});
 
  				//alert(results[i].place_id);
-
 					var request = {
 					placeId: results[i].place_id,
 					fields: ['rating', 'reviews', 'user_ratings_total']
