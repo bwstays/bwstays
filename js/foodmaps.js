bd5329ee8f67b106762bf0de050cd2fe40fd2739
bwstays <bwstays@gmail.com>
2025-08-11 13:37:55 +0530

    Update foodmaps.js

--- js/foodmaps.js
+++ js/foodmaps.js
@@ -83,7 +83,7 @@ document.addEventListener('DOMContentLoaded', () => {
 					let placeHTML = `
 					  <div class="mb-4 text-right">
 						<h6 class="text-white">${place.name}</h6>
-						<a href="https://www.google.com/maps/place/?q=place_id:${detailsRequest.placeId}" alt="location map"> <p class="text-white-50 small mb-1">
+						<a href="https://www.google.com/maps/place/?q=place_id:${detailsRequest.placeId}" alt="location map" target="_blank" rel="noopener noreferrer nofollow" > <p class="text-white-50 small mb-1">
 						  <i class="fas fa-map-marker-alt text-primary mr-2">
 
 						  </i>
