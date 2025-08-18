abdcce85bb8f5c7450a27d2647e03141ef510375
bwstays <bwstays@gmail.com>
2025-08-18 14:17:51 +0530

    Update gas-ev-maps.js

--- js/gas-ev-maps.js
+++ js/gas-ev-maps.js
@@ -57,7 +57,8 @@ document.addEventListener('DOMContentLoaded', () => {
   const request = {
     location: centerloca,
     radius: 40000, // Search within a 40km radius
-    types: ['electric_charging_station','gas_station']
+   // types: ['electric_charging_station','gas_station']
+    types: "electric_charging_station",
   };
 /* const cityCircle = new google.maps.Circle({
         strokeColor: "#FF0000", // Red outline
