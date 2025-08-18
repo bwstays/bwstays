c4602ed3d1859920084febf333ca413731274ada
bwstays <bwstays@gmail.com>
2025-08-18 15:09:49 +0530

    Update gas-ev-maps.js

--- js/gas-ev-maps.js
+++ js/gas-ev-maps.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const centerloca = { lat, lng };
 
   const customIconPump = {
-    url: 'https://www.bwstays.com/assets/img/icons/pin.png',
+    url: 'https://www.bwstays.com/assets/img/logo/pin.png',
     size: new google.maps.Size(30, 30),
     origin: new google.maps.Point(0, 0),
     anchor: new google.maps.Point(20, 30)
@@ -18,9 +18,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const customIconEv = {
     url: 'https://www.bwstays.com/assets/img/icons/ev.png',
-    size: new google.maps.Size(5, 5),
+    size: new google.maps.Size(10, 10),
     origin: new google.maps.Point(0, 0),
-    anchor: new google.maps.Point(5, 5)
+    anchor: new google.maps.Point(20, 30)
   };
 
 
