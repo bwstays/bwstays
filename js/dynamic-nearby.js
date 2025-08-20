1a52abae16c972998233432582a206648804691b
bwstays <bwstays@gmail.com>
2025-08-20 18:24:41 +0530

    Update dynamic-nearby.js

--- js/dynamic-nearby.js
+++ js/dynamic-nearby.js
@@ -124,11 +124,12 @@ function getNearbyPlaces(locationId, maxDistance = maxDistance) {
 
 					var thisId=findPlaceById(i+1);
 
+					console.log(thisId);
 					// alert(locations.names[i]);
 					nearbyPlaces.push({
 						id: i,
 						name: locations.names[i+1],
-						distance: distances[i ],
+						distance: distances[i],
 						image: thisId.image[0],
 						rating: thisId.rating,
 						timing: thisId.timing,
