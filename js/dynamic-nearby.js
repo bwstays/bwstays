30b669c54f9b2d12692d230c3db76328cca49389
bwstays <bwstays@gmail.com>
2025-08-21 10:25:20 +0530

    Update dynamic-nearby.js

--- js/dynamic-nearby.js
+++ js/dynamic-nearby.js
@@ -39,10 +39,10 @@ function findPlaceById(id) {
     const categories = ['plantation', 'waterfalls', 'museums', 'romantic', 'culthist', 'trucking', 'wildlife', 'farms', 'sporting', 'food', 'tribal', 'cycling', 'shopping','pilgrimage'];
     console.log("333333333333" );
     for (const category of categories) {
-		    console.log("4444444444" );
+		    console.log("4444444444" +category );
 
         if (siteData[category]) {
-					    console.log("66666666" );
+					    console.log("66666666" + id + " check id " );
 
             const place = siteData[category].find(p => p.id === id);
 
