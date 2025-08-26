e1c0f60ae1c8a0d4276341a09c53974441d45489
bwstays <bwstays@gmail.com>
2025-08-26 16:44:34 +0530

    updated contact

--- sw.js
+++ sw.js
@@ -223,11 +223,12 @@ function precacheUrl(url) {
     }
 }
 
-if ('storage' in navigator && 'estimate' in navigator.storage) {
+/*if ('storage' in navigator && 'estimate' in navigator.storage) {
   navigator.storage.estimate().then(({ usage, quota }) => {
     console.log(`Using ${usage} out of ${quota} bytes.`);
   });
 }
+*/
 
 self.addEventListener(
     'install', event => {
