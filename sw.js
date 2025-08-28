e45876a07a24d67d03a844458af64e4a287df772
bwstays <bwstays@gmail.com>
2025-08-28 10:21:55 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ var cacheName = 'cache-v4';
 
 //Files to save in cache
 var files = [
-  './',
+ // './',
   './index.html', //SW treats query string as new request
   'https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i&display=swap',
   './css/styles.css',
