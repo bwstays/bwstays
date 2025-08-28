3d440a77a399054e96714059a09a655a9082c5f1
bwstays <bwstays@gmail.com>
2025-08-28 19:15:31 +0530

    cache updated

--- sw.js
+++ sw.js
@@ -104,18 +104,22 @@ async function networkFirst(request) {
 	  const requestUrl = request.url;
 
  if (TILE_URL_PATTERN.test(requestUrl)) {
-	 console.log("from network true");
+	 console.log("from network true" );
 
 
   }
 
   const dynamicCache = await caches.open(cacheName);
+   console.log("from network dynamicCache opened" );
+
   try {
     const networkResponse = await fetch(request);
     // Cache the dynamic API response
     dynamicCache.put(request, networkResponse.clone()).catch((err) => {
       console.warn(request.url + ': ' + err.message);
     });
+       console.log("from network dynamicCache put over" );
+
     return networkResponse;
   } catch (err) {
     const cachedResponse = await dynamicCache.match(request);
