bb40647c95487d497ab8a6cdcc3f6d703551f132
bwstays <bwstays@gmail.com>
2025-09-02 15:42:10 +0530

    mail

--- js/email.js
+++ js/email.js
@@ -8,7 +8,7 @@
          script.onload = function () {
            emaildecode(entry.target)
          };
-         script.src = 'decode-email.js';
+         script.src = 'js/decode-email.js';
          document.head.appendChild(script);
        }
      });
