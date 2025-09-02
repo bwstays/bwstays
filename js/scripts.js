dfa39641b2583237b1ecce76e700d190029d3139
bwstays <bwstays@gmail.com>
2025-09-02 15:26:16 +0530

    email obfus

--- js/scripts.js
+++ js/scripts.js
@@ -81,3 +81,18 @@
          behavior: 'smooth'
      });
  });
+
+
+  const emailtag = document.querySelector('.email-hidden');
+   let observer = new IntersectionObserver((entries) => {
+     entries.map((entry) => {
+       if (entry.isIntersecting) {
+         let script = document.createElement('script');
+         script.onload = function () {
+           emaildecode(entry.target)
+         };
+         script.src = 'decode-email.js';
+         document.head.appendChild(script);
+       }
+     });
+   }).observe(emailtag);
