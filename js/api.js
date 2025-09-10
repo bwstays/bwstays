e70e47c961ee6f7baa750082777b91de30fb7949
bwstays <bwstays@gmail.com>
2025-09-10 11:02:48 +0530

    updates

--- js/api.js
+++
@@ -1,7 +0,0 @@
-$.ajax({
-    url: "https://raw.githubusercontent.com/bwstays/bwstays/refs/heads/main/data/sitedata.js",
-    dataType: "json"
-}).done(function(result){
-  alert( result.siteData) ;
-  console.log(result[0][0]);
-});
