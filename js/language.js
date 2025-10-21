85f36b149111c456a7883b92facc84d85e02714d
bwstays <bwstays@gmail.com>
2025-10-21 20:55:05 +0530

    lang

--- js/language.js
+++ js/language.js
@@ -189,9 +189,9 @@ $(document).ready(function() {
   langManager.init();
 
   // Optional: Listen for language change events
-  /*$(document).on('languageChanged', function(e, lang) {
+ $(document).on('languageChanged', function(e, lang) {
     console.log('Language changed to:', lang);
     // You can add custom logic here when language changes
   });
-  */
+
 });
