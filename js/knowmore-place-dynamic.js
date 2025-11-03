c00071107e2e8f9f045dc95c4e12664cf159fe74
bwstays <bwstays@gmail.com>
2025-11-03 16:39:26 +0530

    Update knowmore-place-dynamic.js

--- js/knowmore-place-dynamic.js
+++ js/knowmore-place-dynamic.js
@@ -463,7 +463,7 @@ function initializeSection(sectionType, data, sectionIndex) {
   const titleElement = document.createElement('h4');
   titleElement.className = 'text-white';
   titleElement.textContent = sectionTitle;
-  titleElement.setAttribute('data-i18n'+sectionIndex, 'translation_key');
+   titleElement.textContent = titleElement.getAttribute('data-i18n'+sectionIndex);
 
   textContainer.appendChild(titleElement);
   const line = document.createElement('div');
